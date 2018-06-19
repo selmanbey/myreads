64adea2f74813b245568bac9f2b53e422beaed65
selmanbey <selmannur@yahoo.com>
2018-06-19 04:11:53 +0100

    adds message to empty search screen

--- src/searchBooks.js
+++ src/searchBooks.js
@@ -102,24 +102,22 @@ class SearchBooks extends Component {
         <div className="search-books-bar">
           <Link className="close-search" to="/" onClick= { this.props.onRefresh }> Close </Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
             <input type="text"
               placeholder="Search by title or author"
               value={query}
               onChange={ (e) => {this.handleChange(e.target.value)} }/>
-
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            { template }
+            { this.state.query ? template :
+              <div className="empty-search-message">
+                This app uses the API and the database provided by Udacity to search and retrieve books.
+                Thus, the search is limited to a particular set of search terms.
+                You can find these search terms here: &nbsp;
+                <a href="https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md"> Search Terms </a>
+              </div>
+             }
           </ol>
         </div>
       </div>
